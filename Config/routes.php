37530e3e58021665fed69e3a5de4e07a5f64b34e
ducng <duc.nguyen@spc-vn.com>
2017-02-15 09:40:12 +0700

    Update routes.php

--- Config/routes.php
+++ Config/routes.php
@@ -26,12 +26,12 @@
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
   //Router::connect('/', array('controller' => 'user', 'action' => 'index'));
-    Router::connect('/', array('controller' => 'client', 'action' => 'index'));
+    Router::connect('/', array('controller' => 'clients', 'action' => 'index'));
     // Router::connect('scenes', array('controller' => 'sitelist', 'action' => 'index'));
     //Router::connect('sample', array('controller' => 'sample', 'action' => 'index'));
     //Router::connect('site', array('controller' => 'site', 'action' => 'index'));
     //Router::connect('/',array('controller' => 'top', 'action' => 'index'));
-    //Router::connect('/home/',array('controller' => 'home', 'action' => 'index'));
+    //Router::connect('/home/',array('controller' => 'home's, 'action' => 'index'));
 
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
