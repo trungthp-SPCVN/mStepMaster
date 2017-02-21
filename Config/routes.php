76606d20973336f4553723a7e7e64be3817c472e
HungNguyen <hungng@spc-vn.com>
2017-02-21 17:54:36 +0700

    Before merge code

--- Config/routes.php
+++ Config/routes.php
@@ -25,7 +25,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-  //Router::connect('/', array('controller' => 'user', 'action' => 'index'));
+    //Router::connect('/', array('controller' => 'user', 'action' => 'index'));
     Router::connect('/', array('controller' => 'clients', 'action' => 'index'));
     // Router::connect('scenes', array('controller' => 'sitelist', 'action' => 'index'));
     //Router::connect('sample', array('controller' => 'sample', 'action' => 'index'));
